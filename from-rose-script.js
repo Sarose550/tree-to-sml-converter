ae62ae649ef35eb5e5aa23ba71aec7c2026ec856
leesue630 <lee.sue630@gmail.com>
2021-04-24 16:00:56 -0400

    finished rose list

--- from-rose-script.js
+++ from-rose-script.js
@@ -1,43 +1,57 @@
-function getRoseValue(indexList) {
-  return document.getElementById("rose_value" + indexList.join(""));
+function getRoseValue(indicesStr) {
+  return document.getElementById("rose_value" + indicesStr).value;
 }
 
-function getRoseChildCountValue(indexList) {
-  return document.getElementById("rose_child_count" + indexList.join(""));
+function getRoseChildCountValue(indicesStr) {
+  return document.getElementById("rose_child_count" + indicesStr).value;
 }
 
-function getCurrRoseChildCount(indexList) {
-  return document.getElementById("rose_children_row" + indexList.join("")).cells
-    .length;
+function getRoseChildRow(indicesStr) {
+  return document.getElementById("rose_children_row" + indicesStr);
 }
 
-function getRoseChildRow(indexList) {
-  return document.getElementById("rose_children_row" + indexList.join(""));
+function getRoseCell(indicesStr) {
+  return document.getElementById("cell" + indicesStr);
 }
 
 /**
  * Clears the table of all its children.
  */
 
-function createGenerateRoseChildrenFunction(rootIndexList) {
-  return generateRoseChildren(rootIndexList);
+function createGenerateRoseChildrenFunction(rootIndicesStr) {
+  return function () {
+    generateRoseChildren(rootIndicesStr);
+  };
 }
 
-function generateRoseChildren(rootIndexList) {
-  console.log(rootIndexList);
-  let childCount = getRoseChildCountValue(rootIndexList);
-  let childRow = getRoseChildRow(rootIndexList);
+function generateRoseChildren(rootIndicesStr) {
+  // console.log("rootIndicesStr", rootIndicesStr);
+  var childCount = getRoseChildCountValue(rootIndicesStr);
+  document.getElementById("rose_root" + rootIndicesStr).colSpan =
+    childCount > 0 ? childCount : 1;
+  // console.log("childCount", childCount);
+  var childRow = getRoseChildRow(rootIndicesStr);
+  // console.log("childRow count", childRow.cells.length);
   if (childCount < childRow.cells.length) {
+    // console.log("remove children");
     while (childCount < childRow.cells.length) {
+      // console.log("removed a child");
       childRow.removeChild(childRow.lastChild);
     }
-  } else {
-    let i = childRow.cells.length;
+  } else if (childCount > childRow.cells.length) {
+    // console.log("add children");
+    var i = childRow.cells.length;
     for (; i < childCount; i++) {
-      let cell = row.insertCell();
+      // console.log("added a child");
+      var cell = childRow.insertCell();
       setAttributes(cell, { colSpan: 1, align: "center" });
-      cell.appendChild(newRoseCell([...rootIndexList].push(i)));
+      cell.appendChild(newRoseCell(rootIndicesStr + i));
+      document.getElementById(
+        "rose_child_count" + rootIndicesStr + i
+      ).oninput = createGenerateRoseChildrenFunction(rootIndicesStr + i);
     }
+  } else {
+    // console.log("correct");
   }
 }
 
@@ -48,14 +62,13 @@ function setAttributes(el, attrs) {
 }
 
 function printHello(indicesStr) {
-  console.log(ev);
   console.log("hello" + indicesStr);
 }
 
 function createPrintHello(indicesStr) {
-    return function () {
-        printHello(indicesStr);
-    }
+  return function () {
+    printHello(indicesStr);
+  };
 }
 
 function newRoseValueInput(indicesStr) {
@@ -71,15 +84,16 @@ function newRoseValueInput(indicesStr) {
 
 function newRoseChildCountInput(indicesStr) {
   var childCountValue = document.createElement("input");
-  childCountValue.type = "number";
-  childCountValue.size = 3;
-  childCountValue.min = 0;
-  childCountValue.max = 5;
-  childCountValue.id = "rose_child_count" + indicesStr;
-  childCountValue.value = 0;
-  childCountValue.placeholder = 0;
-  childCountValue.oninput = createPrintHello(indicesStr);
-  // childCountValue.oninput = createGenerateRoseChildrenFunction(indexList);
+  setAttributes(childCountValue, {
+    type: "number",
+    size: 3,
+    min: 0,
+    max: 5,
+    id: "rose_child_count" + indicesStr,
+    value: 0,
+    placeholder: 0,
+  });
+  // childCountValue.oninput = createGenerateRoseChildrenFunction(indicesStr);
   return childCountValue;
 }
 
@@ -92,9 +106,13 @@ function newRoseChildCountInput(indicesStr) {
 function newRoseCell(indicesStr) {
   // create table container
   var table = document.createElement("table");
-  let row = table.insertRow();
-  let cell = row.insertCell();
-  setAttributes(cell, { colSpan: 1, align: "center" });
+  var row1 = table.insertRow();
+  var cell = row1.insertCell();
+  setAttributes(cell, {
+    colSpan: 1,
+    align: "center",
+    id: "rose_root" + indicesStr,
+  });
 
   var div = document.createElement("div");
   setAttributes(div, { id: "cell" + indicesStr, align: "left" });
@@ -102,56 +120,89 @@ function newRoseCell(indicesStr) {
   colorCell(div, "base");
   div.innerHTML += "Rose: ";
 
-  div.appendChild(newRoseValueInput(indicesStr));
+  var roseValueInput = newRoseValueInput(indicesStr);
+  div.appendChild(roseValueInput);
 
   div.innerHTML += "<br/>";
 
-  div.appendChild(newRoseChildCountInput(indicesStr));
+  var roseChildCountInput = newRoseChildCountInput(indicesStr);
+  div.appendChild(roseChildCountInput);
   div.innerHTML += " children";
 
   cell.appendChild(div);
 
-  let row2 = table.insertRow();
+  var row2 = table.insertRow();
   setAttributes(row2, {
     id: "rose_children_row" + indicesStr,
+    valign: "top",
   });
 
   return table;
 }
 
+function addRoseToCell(cell, roseCell) {
+  cell.appendChild(roseCell);
+}
+
 function generateRoseTable() {
   // console.log("generateInputTable for rose");
-  let row = table.insertRow();
-  let cell = row.insertCell();
+  clearTable();
+  var row = table.insertRow();
+  var cell = row.insertCell();
   setAttributes(cell, { colSpan: 1, align: "center" });
   cell.appendChild(newRoseCell("0"));
+  document.getElementById(
+    "rose_child_count0"
+  ).oninput = createGenerateRoseChildrenFunction("0");
 }
 
 /**
- * Called when 'Generate SML Text' button clicked
+ * Returns the SML version of the rose rooted at cell indicesStr
+ * @param indicesStr The string of concatenated indices
+ */
+function roseTextHelper(indicesStr) {
+  var roseCellValue = getRoseValue(indicesStr);
+  colorCell(getRoseCell(indicesStr), "valid");
+  var text = "Rose(" + roseCellValue + ",[";
+  if (roseCellValue.includes("-"))
+    setNegativeWarningText("Warning: Negative sign used instead of ~");
+  if (roseCellValue === "")
+    setTableWarningText("Warning: Empty rose value");
+  for (var i = 0; i < getRoseChildCountValue(indicesStr); i++) {
+    text += roseTextHelper(indicesStr + i);
+    if (i < getRoseChildCountValue(indicesStr) - 1) {
+      text += ",";
+    }
+  }
+  text += "])";
+  return text;
+}
+
+/**
+ * Called when 'Generate SML Text' button clicked for "rose" tab
  */
 function generateRoseText() {
   // // reset error text
-  // resetWarningText();
+  resetWarningText();
 
   console.log("generate rose text");
 
   // uncolor all input borders
-  //   for (var i = 0; i < depth; i++) {
-  //     for (var j = 0; j < Math.pow(2, i); j++) {
-  //       uncolorInputBorder(i, j);
-  //     }
+  // for (var i = 0; i < depth; i++) {
+  //   for (var j = 0; j < Math.pow(2, i); j++) {
+  //     uncolorInputBorder(i, j);
   //   }
+  // }
 
-  //   // compute SML text from tree/shrub
-  //   var text;
-  //   try {
-  //     text = treeTextHelper(0, 0);
-  //   } catch (e) {
-  //     console.log(e);
-  //     text = "Node must have 2 valid children.";
-  //   }
+  // compute SML text from tree/shrub
+  var text;
+  try {
+    text = roseTextHelper("0");
+  } catch (e) {
+    console.log(e);
+    text = "Something went wrong...";
+  }
 
-  //   // set SML output text
-  //   setSMLOutputText(text);
+  // set SML output text
+  setSMLOutputText(text);
 }
